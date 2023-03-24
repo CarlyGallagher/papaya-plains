2db06d8f2c53365b746c25c5c6caab4bf51f3c01
CarlyGallagher <jagllaghercarly3@yahoo.com>
2023-03-23 18:54:16 -0700

    created connection

--- index.js
+++ index.js
@@ -2,3 +2,18 @@ const inquirer = require('inquirer');
 const cTable = require('inquirer');
 const mysql = require('mysql2');
 
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "root",
+    database: "employeeTracker_db"
+});
+
+connection.connect(function (err) {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+
+});
