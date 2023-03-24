a05ca9a6e0bf0a76d73990f8dbb30e0d964e00f7
CarlyGallagher <jagllaghercarly3@yahoo.com>
2023-03-23 19:03:53 -0700

    updated schema password

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
     host: "localhost",
-    port: 3306,
+    port: 3001,
     user: "root",
-    password: "root",
+    password: "flea",
     database: "employeeTracker_db"
 });
 
